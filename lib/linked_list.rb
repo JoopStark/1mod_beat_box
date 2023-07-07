e44de5ce6beaa4be4018a30a16bfaee027ed8542
Joop Stark <stark.joop@gmail.com>
2023-07-07 15:28:45 -0600

    Correct #count and #append to be more universal

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,25 +4,45 @@ class LinkedList
     @head = nil
   end
 
+  # def location
+  #   if @head.next_node.nil?
+  #     :thehead
+  #   elsif 
+
 
 
   def append(sound)
-    if @head
-      @head.append_next_node(sound) 
+    new_node = Node.new(sound)
+    if @head.nil?
+      @head = new_node
     else
-      @head = Node.new(sound)
+      current = @head
+      until current == nil
+        previous = current
+        current = current.next_node
+      end
+      previous.next_node = new_node
     end
   end
 
   def count
     iterator = 0
-    if @head != nil
+    current = @head
+    until current == nil
+      current = current.next_node
       iterator += 1
     end
+    iterator
   end
 
   def to_string
-    @head.to_string
+    the_string = ""
+    current = @head
+    until current == nil
+      the_string << current.data
+      current = current.next_node
+    end
+    the_string
   end
 
 
