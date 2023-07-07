983c54598d4308ff19e9bfe1685c217a61addc59
Joop Stark <stark.joop@gmail.com>
2023-07-07 13:55:19 -0600

    Complete add muliple nodes test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,42 +2,64 @@ require './lib/node'
 require './lib/linked_list'
 
 describe LinkedList do
-  context "setup" do
-    it "has nil head" do
-      list = LinkedList.new
+  context "single node" do
+    context "setup" do
+      it "has nil head" do
+        list = LinkedList.new
 
-      expect(list.head).to eq(nil)
+        expect(list.head).to eq(nil)
+      end
     end
-  
+
+    context "#append" do
+      it "can recieve sound as node" do
+        list = LinkedList.new
+
+        list.append("doop")
+
+        expect(list.head.data).to eq("doop")
+      end
+    end
+
+    context "#count" do
+      it "can recieve sound as node" do
+        list = LinkedList.new
+
+        list.append("doop")
+
+        expect(list.count).to eq(1)
+      end
+    end
+
+    context "#to_string" do
+      it "can recieve sound as node" do
+        list = LinkedList.new
+
+        list.append("doop")
+
+        expect(list.to_string).to eq("doop")
+      end
+    end
   end
 
-  context "#append" do
-    it "can recieve sound as node" do
+
+  context "multiple nodes" do 
+    it "can add multiple nodes" do
       list = LinkedList.new
 
       list.append("doop")
 
       expect(list.head.data).to eq("doop")
+
+      list.append("deep")
+
+      expect(list.head.next_node.data).to eq("deep")
     end
   end
 
-  context "#count" do
-    it "can recieve sound as node" do
-      list = LinkedList.new
 
-      list.append("doop")
 
-      expect(list.count).to eq(1)
-    end
-  end
 
-  context "#to_string" do
-    it "can recieve sound as node" do
-      list = LinkedList.new
 
-      list.append("doop")
 
-      expect(list.count).to eq("doop")
-    end
-  end
 end
