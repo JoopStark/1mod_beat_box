ebfce5539975487a8dc0c3456b5c81475b47966e
Joop Stark <stark.joop@gmail.com>
2023-07-06 20:40:41 -0600

    Build test so nodes can recieve data

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,14 @@
-git
+require './lib/node'
+
+describe Node do
+  context "set-up" do
+    it "recieves data" do
+      node = Node.new("plop")
+
+      expect(node.data).to be("plop")
+    end
+
+    it ""
+
+  end
+end
