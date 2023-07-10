526db45c472de7bbf86c7acee85ab4fc79c0c9b5
Joop Stark <stark.joop@gmail.com>
2023-07-10 14:13:04 -0600

    feat: #append and #prepend only valid sounds

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,12 +1,16 @@
 class Beatbox
   attr_reader :list
- def initialize
-  @list = LinkedList.new
- end
+  def initialize
+    @list = LinkedList.new
+    @BEATBOXSOUNDS = ["tee", "dee", "deep", "bop", "boop", "la", "na", "doo", "woo", "hoo", "shu", "ditt"]
+  end
+  
 
  def append(string)
+  # usables = ["tee", "dee", "deep", "bop", "boop", "la", "na", "doo", "woo", "hoo", "shu", "ditt"]
   sounds = string.split(" ")
-  sounds.each {|sound| list.append(sound)}
+  proper = sounds.select {|word| word if @BEATBOXSOUNDS.include?(word)}
+  proper.each {|sound| list.append(sound)}
  end
 
  def count
@@ -16,6 +20,17 @@ class Beatbox
  def play
   `say -r 500 -v Boing #{@list.to_string}`
  end
+
+ def all
+  @list.to_string
+ end
+
+ def prepend(string)
+  sounds = string.split(" ")
+  proper = sounds.select {|word| word if @BEATBOXSOUNDS.include?(word)}
+  proper.each {|sound| list.prepend(sound)}
+ end
+
 
 
 
