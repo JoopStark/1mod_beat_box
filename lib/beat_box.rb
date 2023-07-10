b839d6e06deadc2d5587fc4c3ca523b905a0ee99
Joop Stark <stark.joop@gmail.com>
2023-07-10 15:52:58 -0600

    feat: private #clean for #prepend and #append

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,44 +4,32 @@ class Beatbox
     @list = LinkedList.new
     @BEATBOXSOUNDS = ["tee", "dee", "deep", "bop", "boop", "la", "na", "doo", "woo", "hoo", "shu", "ditt"]
   end
-  
 
- def append(string)
-  # usables = ["tee", "dee", "deep", "bop", "boop", "la", "na", "doo", "woo", "hoo", "shu", "ditt"]
-  sounds = string.split(" ")
-  proper = sounds.select {|word| word if @BEATBOXSOUNDS.include?(word)}
-  proper.each {|sound| list.append(sound)}
- end
+  def append(string)
+    clean(string).each {|sound| list.append(sound)}
+  end
 
- def count
-  @list.count
- end
+  def count
+    @list.count
+  end
 
- def play
-  `say -r 500 -v Boing #{@list.to_string}`
- end
+  def play
+    `say -r 500 -v Boing #{@list.to_string}`
+  end
 
- def all
-  @list.to_string
- end
+  def all
+    @list.to_string
+  end
 
- def prepend(string)
-  sounds = string.split(" ")
-  proper = sounds.select {|word| word if @BEATBOXSOUNDS.include?(word)}
-  proper.each {|sound| list.prepend(sound)}
- end
+  def prepend(string)
+    clean(string).each {|sound| list.prepend(sound)}
+  end
 
+  private
 
-
-
-
-
-
-
-
-
-
-
-
-
+  def clean(object)
+    string = object.to_s
+    sounds = string.split(" ")
+    proper = sounds.select {|word| word if @BEATBOXSOUNDS.include?(word)}
+  end
 end
